fa636fdabb01b29893180d2d8ba5d5272422d862
Aung Ko Man <aungkoman@gmail.com>
2023-08-28 08:37:24 +0630

    user profile model set

--- twitter/urls.py
+++ twitter/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     # this is new line to add
     path("", include("users.urls")),
     path("admin/", admin.site.urls)
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class UserProfileInfo(models.Model):  
-    user = models.OneToOneField(User,on_delete=models.CASCADE)  
-    username = models.CharField(max_length=25,null=True)  
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
     dob = models.DateField(blank=True,null=True) # If no date is selected then Django saves blank field value.  
     city = models.CharField(max_length=25,blank=True,null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
